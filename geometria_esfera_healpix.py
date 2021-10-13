0ec405cf765dde592e4ac0f42a32b62546a407b4
MagnoOcarlos <76740623+MagnoOcarlos@users.noreply.github.com>
2021-10-13 15:55:55 -0300

    Update geometria_esfera_healpix.py

--- geometria_esfera_healpix.py
+++ geometria_esfera_healpix.py
@@ -5,8 +5,11 @@ import os.path      #teste de existência de arquivos
 import sys          #leitura via terminal
 
 
+
+
+
 #leitura dos dados da simulção isotrópica no CRPropa
-data = pd.read_csv('dados.txt',header=0, sep = '\s+', dtype ='a')
+data = pd.read_csv(sys.argv[1],header=0, sep = '\s+', dtype ='a')
 
 data = data.astype(float)
 
@@ -26,27 +29,27 @@ r = 3.8 #Mpc
 pixel = np.loadtxt("Pixels_Carlos.txt")
 
 
-final = None
+final_eventos = None
+final_cone = None
 
 for i in range(0,3072):
 
     #Um ponto na esfera (também o vetor Origem -> Ponto)(coordenadas esféricas para cartesianas)
-    x = r*np.cos(pixel[i,1])*np.sin(pixel[i,2])
-    y = r*np.sin(pixel[i,1])*np.sin(pixel[i,2])
-    z = r*np.cos(pixel[i,2])
-
+    x = r*np.cos(np.radians(pixel[i,1]))*np.sin(np.radians(pixel[i,2]))
+    y = r*np.sin(np.radians(pixel[i,1]))*np.sin(np.radians(pixel[i,2]))
+    z = r*np.cos(np.radians(pixel[i,2]))
 
 
     #Angulo entre a linha de visada e os ângulos de emissão de cada evento
 
     #produto interno
-    p_int_px = ( (x * data.Px) + (y * data.Py) + (z * data.Pz) )
+    p_int_px = ( (x * data.P0x) + (y * data.P0y) + (z * data.P0z) )
 
     #norma vetor linha divisada
     norma_p = np.sqrt( np.power(x,2) + np.power(y,2) + np.power(z,2))
 
     #Norma vetor direção de emissão
-    norma_px = np.sqrt( ( np.power(data.Px,2) ) + ( np.power(data.Py,2) )+ ( np.power(data.Pz,2) ) )
+    norma_px = np.sqrt( ( np.power(data.P0x,2) ) + ( np.power(data.P0y,2) )+ ( np.power(data.P0z,2) ) )
 
 
     ang =  p_int_px / (norma_px * norma_p)
@@ -60,9 +63,9 @@ for i in range(0,3072):
     data['Ang_linha_de_visada'] = np.degrees( np.arccos( ang ) )
 
 
-    #criando o cone
+#criando o cone
 
-    ang_linha_centro_cone= int(sys.argv[1])
+    ang_linha_centro_cone= int(sys.argv[2])
     ang_cone=5
 
     sup_ang_cone =ang_linha_centro_cone + ang_cone/2
@@ -76,12 +79,13 @@ for i in range(0,3072):
     test = (data.Ang_linha_de_visada <= sup_ang_cone) & (data.Ang_linha_de_visada >= inf_ang_cone)
     cut = data[test]
 
+
     """
         A variável cut guarda todos os eventos que tiveram como retorno True no test
         da linha 77.
     """
 
-    #criar uma coluna no cut com o angulo entre a linha divisada e os Vetores Origem -> (x,y,z)
+#criar uma coluna no cut com o angulo entre a linha divisada e os Vetores Origem -> (x,y,z)
     '''
         O raciocínio aqui é criar vetores entre a origem(emissor) e as coordenadas de chegada de cada evento,
         calcular o ângulo desses vetores com o vetor da linha de visada( o pixel das linhas 36,37 e 38) e acres-
@@ -100,9 +104,8 @@ for i in range(0,3072):
 
     cut['Dist_ang'] = np.degrees( np.arccos( ang ) )
 
-
-    #distancia ângular (criação do pixel)
-    cut_dist = (cut.Dist_ang < 3) #pixel com distancia angular de 3 graus
+#distancia ângular (criação do pixel)
+    cut_dist = (cut.Dist_ang <= 0.15) #pixel com distancia angular de 0.1 graus
 
     aux = cut[cut_dist]
     #final = cut[cut_dist]#mesma lógica da linha 78
@@ -112,15 +115,18 @@ for i in range(0,3072):
 
     """
 
-
     if i == 0:
 
-        final = aux
+        final_eventos = aux
+        final_cone = cut
 
     else:
 
-        final = final.append(aux, ignore_index = True)
+        final_eventos = final_eventos.append(aux, ignore_index = True)
+        final_cone = final_cone.append(cut, ignore_index = True)
 
 #fim do for
 
-np.savetxt('test_file_beta.txt', final,comments ='', fmt='%f', delimiter="\t", header="D	\tz	\tSN	\tID	\tE\t	X\t	Y\t	Z	\tPx\t	Py\t	Pz\t	SN0\t	ID0\t	E0\t	X0\t	Y0\t	Z0\t	P0x\t	P0y\t	P0z\t	SN1\t	ID1\t	E1\t	X1\t	Y1\t	Z1\t	P1x\t	P1y\t	P1z\t	W\t Ang_linha_de_visada  \t Dist_ang")
+
+np.savetxt(sys.argv[3], final_eventos,comments ='', fmt='%f', delimiter="\t", header="D \tz     \tSN    \tID    \tE\t   X\t     Y\t     Z       \tPx\t  Py\t    Pz\t    SN0\t   ID0\t   E0\t    X0\t    Y0\t    Z0\t    P0x\t   P0y\t   P0z\t   SN1\t   ID1\t   E1\t    X1\t    Y1\t    Z1\t    P1x\t   P1y\t   P1z\t   W\t Ang_linha_de_visada  \t Dist_ang")
+np.savetxt(sys.argv[4], final_cone,comments ='', fmt='%f', delimiter="\t", header="D    \tz     \tSN    \tID    \tE\t   X\t     Y\t     Z       \tPx\t  Py\t    Pz\t    SN0\t   ID0\t   E0\t    X0\t    Y0\t    Z0\t    P0x\t   P0y\t   P0z\t   SN1\t   ID1\t   E1\t    X1\t    Y1\t    Z1\t    P1x\t   P1y\t   P1z\t   W\t Ang_linha_de_visada ")
